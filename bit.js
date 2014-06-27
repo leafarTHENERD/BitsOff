67df320069d69c11332b5962889c9590dbd38aa7
LeafarTHENERD <leafar.thenerd@hotmail.com>
2014-06-27 14:56:54 -0300

    Adicionei uma função de desenho no objeto bit, que recebe um contexto de canvas como parametro

--- bit.js
+++ bit.js
@@ -1,14 +1,12 @@
 var bit = {
 	position: null,
 	status: 0,
-	height: 0,
-	width: 0,
+	height: 100,
+	width: 100,
 
-	create: function(x, y, h, w, stat){
+	create: function(x, y, stat){
 		var obj = Object.create(this);
 		obj.position = vector.create(x, y);
-		obj.height = h;
-		obj.width = w;
 		obj.status = stat;
 		return obj;
 	},
@@ -19,5 +17,29 @@ var bit = {
 
 	getStatus: function(){
 		return this.status;
+	},
+
+	setHeight: function(h){
+		this.height = h;
+	},
+
+	getHeight: function(){
+		return this.height;
+	},
+
+	setWidth: function(w){
+		this.width = w;
+	},
+
+	getWidth: function(){
+		return this.width;
+	},
+
+	draw: function(ctx){
+		if(this.status)
+			ctx.fillStyle = "#EEE";
+		else
+			ctx.fillStyle = "#000";
+		ctx.fillRect(this.position.getX(), this.position.getY(), this.width, this.height);
 	}
-}
+};
