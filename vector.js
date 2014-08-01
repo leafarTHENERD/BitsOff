def572c9656f47afab39c5a792f5e122a357afeb
leafarTHENERD <leafar.thenerd@hotmail.com>
2014-08-01 12:42:15 -0300

    Retirada de funções não utilizadas

--- vector.js
+++ vector.js
@@ -23,62 +23,6 @@ var vector = {
 
     getY: function(){
         return this._y;
-    },
-
-    setAngle: function(angle){
-        var length = this.getLength();
-        this._x = Math.cos(angle) * length;
-        this._y = Math.sin(angle) * length;
-    },
-
-    getAngle: function(){
-        return Math.atan2(this._y, this._x);
-    },
-
-    setLength: function(length){
-        var angle = this.getAngle();
-        this._x = Math.cos(angle) * length;
-        this._y = Math.sin(angle) * length;
-    },
-
-    getLength: function(){
-        return Math.sqrt(this._x * this._x + this._y * this._y);
-    },
-
-    add: function(v2){
-        return vector.create(this._x + v2.getX(), this._y + v2.getY());
-    },
-
-    subtract: function(v2){
-        return vector.create(this._x - v2.getX(), this._y - v2.getY());
-    },
-
-    multiply: function(val){
-        return vector.create(this._x * val, this._y * val);
-    },
-
-    divide: function(val){
-        return vector.create(this._x / val, this._y / val);
-    },
-
-    addTo: function(v2){
-        this._x += v2.getX();
-        this._y += v2.getY();
-    },
-
-    subtractFrom: function(v2){
-        this._x -= v2.getX();
-        this._y -= v2.getY();
-    },
-
-    multiplyBy: function(val){
-        this._x *= val;
-        this._y *= val;
-    },
-
-    divideBy: function(val){
-        this._x /= val;
-        this._y /= val;
     }
 
 };
